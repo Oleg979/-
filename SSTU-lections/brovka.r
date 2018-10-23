89fe79d691d120417270d649e9c8354d5e28239e
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-10-23 12:10:01 +0400

    Create brovka.r

---
+++ SSTU-lections/brovka.r
@@ -0,0 +1,13 @@
+x1 <- seq(0, 1, length.out = 11)
+x2 <- seq(stats::rnorm(20)) 
+x3 <- seq(1, 9, by = 2)     
+x4 <- seq(1, 9, by = pi)    
+x5 <- seq(1, 6, by = 3)
+x6 <- seq(1.575, 5.125, by = 0.05)
+x7 <- seq(17)
+
+mat <- matrix(data = rnorm (100, mean = 100, sd = 50), nrow = 10, ncol = 10)
+
+x8 <- seq(10)
+
+
