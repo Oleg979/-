e107b00583463a59d29ec6b9d4eeeb7a3f054682
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-10-25 21:41:47 +0400

    Update brovka.r

--- SSTU-lections/brovka.r
+++ SSTU-lections/brovka.r
@@ -1,15 +1,48 @@
 help(seq)
-x1 <- seq(0, 1, length.out = 11)
-x2 <- seq(stats::rnorm(20)) 
-x3 <- seq(1, 9, by = 2)     
-x4 <- seq(1, 9, by = pi)    
-x5 <- seq(1, 6, by = 3)
-x6 <- seq(1.575, 5.125, by = 0.05)
-x7 <- seq(17)
+x1 <- seq(0, 1, length.out = 11) 
+x1 <- seq(stats::rnorm(20)) 
+x2 <- seq(1, 9, by = 2) 
+x3 <- seq(1, 9, by = pi) 
+x4 <- seq(1, 6, by = 3) 
+x5 <- seq(1.575, 5.125, by = 0.05) 
+x6 <- seq(17) 
+save.image("file.RData")
+rm(list=ls())
+load("file.RData") 
 
-A <- matrix(data = rnorm (100, mean = 100, sd = 50), nrow = 10, ncol = 10)
+help(matrix) 
+help(solve)
+?("%*%")
+A <- (matrix(data = rnorm (100, mean = 50, sd = 30), nrow = 10, ncol = 10))
 X <- seq(10)
-mat1 <- A * X
-mat2 <- A %*% X
-B <- c(mat2)
+V <- A*X
+B <- A%*%X
+solve(A,B) 
+ 
+HousePrice <- read.table(file="houses.data") 
+HousePrice1 <- read.table(file="houses1.data, header = TRUE") 
+HP <- edit(HousePrice) 
+write.table(HP, file="myframe.txt", quote = FALSE, row.names = FALSE) 
+HousePrice$Rooms 
+HousePrice1$Rooms 
+HP$Rooms
 
+T <- c(rnorm (100, mean = 50, sd = 30))
+png(filename="1.png")
+plot(T)
+png(filename="2.png") 
+plot(sort(T))
+png(filename="3.png") 
+hist(T)
+png(filename="4.png") 
+plot(density(T))
+png(filename="5.png") 
+hist(T) 
+rug(T)
+dev.off()
+cars
+plot(cars)
+plot(sin, 0, 2*pi)
+plot(cos, 0, 2*pi)
+plot(exp, 0, 5)
+plot(log, 0, 10)
